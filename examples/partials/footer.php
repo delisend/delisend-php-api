55d79ee66fd1d2be3805a1343afd7d51fd566b8c
Peter Rosinský <peter.rosinsky@isklad.eu>
2022-11-05 15:37:14 +0100

    Remove example playground

--- examples/partials/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="my-5 pt-5 text-muted text-center text-small">
-    <p class="mb-1">© 2008–2022 <a target="_blank" href="https://rosigroup.com">Rosi Group s.r.o.</a></p>
-    <ul class="list-inline">
-        <li class="list-inline-item"><a href="#">Privacy</a></li>
-        <li class="list-inline-item"><a href="#">Terms</a></li>
-        <li class="list-inline-item"><a href="#">Support</a></li>
-    </ul>
-</footer>
