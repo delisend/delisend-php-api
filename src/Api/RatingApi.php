e18de3c92e87e83d579f2ad7cea8805e4828df3d
Peter Rosinský <peto.rosinsky@rosigroup.com>
2023-06-12 10:42:42 +0200

    Create new customer rating

--- src/Api/RatingApi.php
+++ src/Api/RatingApi.php
@@ -3,7 +3,7 @@
 namespace DelisendApi\Api;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Psr7\MultipartStream;
 use GuzzleHttp\Psr7\Request;
@@ -13,9 +13,9 @@ use DelisendApi\Configuration;
 use DelisendApi\HeaderSelector;
 use DelisendApi\ObjectSerializer;
 
-
 /**
  * Class RatingApi
+ *
  * @package Delisend\Api
  */
 class RatingApi
@@ -42,11 +42,10 @@ class RatingApi
      * @param HeaderSelector|null $selector
      */
     public function __construct(
-        Client $client = null,
-        Configuration $config = null,
+        Client         $client = null,
+        Configuration  $config = null,
         HeaderSelector $selector = null
-    )
-    {
+    ) {
         $this->client = $client ?: new Client();
         $this->config = $config ?: new Configuration();
         $this->headerSelector = $selector ?: new HeaderSelector();
@@ -62,15 +61,14 @@ class RatingApi
     }
 
     /**
-     * Operation searchContactsListsGet
-     *
+     * Operation get Delisend rating
      * Get list of searched contact list
      *
-     * @param  array $q Keyword or query. (required)
+     * @param array $q Keyword or query. (required)
      *
-     * @throws ApiException on non-2xx response
-     * @throws \InvalidArgumentException
      * @return string
+     * @throws \InvalidArgumentException
+     * @throws ApiException on non-2xx response
      */
     public function ratingGet(array $q)
     {
@@ -80,6 +78,7 @@ class RatingApi
 
     /**
      * @param array $q
+     *
      * @return array
      * @throws ApiException
      * @throws \GuzzleHttp\Exception\GuzzleException
@@ -206,7 +205,8 @@ class RatingApi
 
     /**
      * @param array $q
-     * @param $
+     *
+     * @return Request
      */
     protected function ratingGetRequest(array $q)
     {
@@ -292,7 +292,6 @@ class RatingApi
         );
     }
 
-
     /**
      * Create http client option
      *
@@ -310,5 +309,246 @@ class RatingApi
         }
 
         return $options;
+    }
+
+    /**
+     * Operation createRatingByOrder
+     * Create new customer rating
+     * @params $order Order Detail
+     *
+     * @param $order
+     * @param int $order_id Order id (required)
+     * @param float $rating
+     * @param string $comment
+     *
+     * @return string
+     */
+    public function createRatingByOrder($order, int $order_id, float $rating, string $comment): string
+    {
+        list($response) = $this->createRatingByOrderPostWithHttpInfo($order, $order_id, $rating, $comment);
+        return $response;
+    }
+
+    /**
+     * @param $order
+     * @param $order_id
+     * @param $rating
+     * @param $comment
+     *
+     * @return array of string, HTTP status code, HTTP response headers (array of strings)
+     * @throws ApiException
+     * @throws GuzzleException
+     */
+    public function createRatingByOrderPostWithHttpInfo($order, $order_id, $rating, $comment): array
+    {
+        $returnType = 'string';
+        $request = $this->createRatingByOrderPostRequest($order, $order_id, $rating, $comment);
+
+        try {
+            $options = $this->createHttpClientOption();
+            try {
+                $response = $this->client->send($request, $options);
+            } catch (RequestException $e) {
+                throw new ApiException(
+                    "[{$e->getCode()}] {$e->getMessage()}",
+                    $e->getCode(),
+                    $e->getResponse() ? $e->getResponse()->getHeaders() : null,
+                    $e->getResponse() ? $e->getResponse()->getBody()->getContents() : null
+                );
+            }
+
+            $statusCode = $response->getStatusCode();
+
+            if ($statusCode < 200 || $statusCode > 299) {
+                throw new ApiException(
+                    sprintf(
+                        '[%d] Error connecting to the API (%s)',
+                        $statusCode,
+                        $request->getUri()
+                    ),
+                    $statusCode,
+                    $response->getHeaders(),
+                    $response->getBody()
+                );
+            }
+
+            $responseBody = $response->getBody();
+            if ($returnType === '\SplFileObject') {
+                $content = $responseBody; //stream goes to serializer
+            } else {
+                $content = $responseBody->getContents();
+                if ($returnType !== 'string') {
+                    $content = json_decode($content);
+                }
+            }
+
+            return [
+                ObjectSerializer::deserialize($content, $returnType, []),
+                $response->getStatusCode(),
+                $response->getHeaders()
+            ];
+
+        } catch (ApiException $e) {
+            switch ($e->getCode()) {
+                case 200:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        'string',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 400:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        'string',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 401:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        'string',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 403:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        'string',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 404:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        'string',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 405:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        'string',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 429:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        'string',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                default:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        'string',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+            }
+            throw $e;
+        }
+    }
+
+    /**
+     * @param $order
+     * @param int $order_id
+     * @param float $rating
+     * @param string $comment
+     *
+     * @return Request
+     */
+    protected function createRatingByOrderPostRequest($order, int $order_id, float $rating, string $comment): Request
+    {
+        // verify the required parameter 'contact' is set
+        if ($order === null || (is_array($order) && count($order) === 0)) {
+            throw new \InvalidArgumentException(
+                'Missing the required parameter $contact when calling listsContactsByListIdPost'
+            );
+        }
+        // verify the required parameter 'list_id' is set
+        if ($order_id === null || (is_array($order_id) && count($order_id) === 0)) {
+            throw new \InvalidArgumentException(
+                'Missing the required parameter $list_id when calling listsContactsByListIdPost'
+            );
+        }
+
+        // body params
+        $_tempBody = null;
+
+        $resourcePath = '/rating/create';
+        $formParams = $order;
+        $queryParams = [];
+        $headerParams = [];
+        $httpBody = '';
+        $multipart = false;
+
+        if ($multipart) {
+            $headers = $this->headerSelector->selectHeadersForMultipart(
+                ['application/json']
+            );
+        } else {
+            $headers = $this->headerSelector->selectHeaders(
+                ['application/json'],
+                ['application/json']
+            );
+        }
+
+        // for model (json/xml)
+        if (isset($_tempBody) || $_tempBody !== null) {
+            // $_tempBody is the method argument, if present
+            $httpBody = $_tempBody;
+
+            if ($headers['Content-Type'] === 'application/json') {
+                // \stdClass has no __toString(), so we should encode it manually
+                if ($httpBody instanceof \stdClass) {
+                    $httpBody = \GuzzleHttp\Utils::jsonEncode($httpBody);
+                }
+                // array has no __toString(), so we should encode it manually
+                if (is_array($httpBody)) {
+                    $httpBody = \GuzzleHttp\Utils::jsonEncode(ObjectSerializer::sanitizeForSerialization($httpBody));
+                }
+            }
+        } elseif (count($formParams) > 0) {
+            if ($headers['Content-Type'] === 'application/json') {
+                $httpBody = \GuzzleHttp\Utils::jsonEncode($formParams);
+            } else {
+                // for HTTP post (form)
+                $httpBody = \GuzzleHttp\Psr7\Query::build($formParams);
+            }
+        }
+
+        // this endpoint requires OAuth 2.0 Bearer Token authentication
+        if ($this->config->getUsername() !== null || $this->config->getPassword() !== null) {
+            $headers['Authorization'] = 'Bearer ' . $this->config->getAccessToken();
+        }
+
+        $defaultHeaders = [];
+        if ($this->config->getUserAgent()) {
+            $defaultHeaders['User-Agent'] = $this->config->getUserAgent();
+        }
+
+        $headers = array_merge(
+            $defaultHeaders,
+            $headerParams,
+            $headers
+        );
+
+        $query = \GuzzleHttp\Psr7\Query::build($queryParams);
+        return new Request(
+            'POST',
+            $this->config->getHost() . $resourcePath . ($query ? "?{$query}" : ''),
+            $headers,
+            $httpBody
+        );
     }
 }
